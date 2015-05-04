63d7b2e6641f0aaf1382141f9714c82313da4df0
matthew.lu <matthew.lu@permissioncorp.com>
2015-05-04 18:52:39 +0800

    Add shopping cart-Part one

---
+++ Controllers/CartController.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using AoqibaoStore.Abstract;
+using AoqibaoStore.Models;
+
+namespace AoqibaoStore.Controllers
+{
+    public class CartController : Controller
+    {
+
+        private IProductRepository repository;
+        public CartController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel { 
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+
+        public RedirectToRouteResult AddToCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products
+                              .FirstOrDefault(p=> p.Id == productId);
+            if (product != null)
+            {
+                GetCart().AddItem(product,1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+        {
+            Product product = repository.Products
+                              .FirstOrDefault(p=>p.Id == productId);
+            if (product != null)
+            {
+                GetCart().RemoveLine(product);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        private Cart GetCart() { 
+        
+            Cart cart= (Cart)Session["Cart"];
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+
+
+    }
+}
---
+++ Models/Cart.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+
+namespace AoqibaoStore.Models
+{
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        public void AddItem(Product product, int quantity)
+        {
+            CartLine line = lineCollection
+                            .Where(p => p.Product.Id == product.Id)
+                            .FirstOrDefault();
+
+            if (line == null)
+            {
+                lineCollection.Add(new CartLine { 
+                  Product = product,
+                  Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public void RemoveLine(Product product)
+        {
+            lineCollection.RemoveAll(l=>l.Product.Id == product.Id);
+        }
+
+        public decimal ComputeTotalValue() {
+            return lineCollection.Sum(e => e.Product.unitPrice * e.Quantity);
+        }
+
+
+        public void Clear()
+        {
+            lineCollection.Clear();
+        }
+
+        public IEnumerable<CartLine> Lines {
+            get { return lineCollection; }
+        }
+    }
+
+    public class CartLine {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+
+}
